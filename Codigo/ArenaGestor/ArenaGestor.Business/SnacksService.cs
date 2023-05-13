e083dee3bb32c78d5ad543ed1918b64fccba2ff3
Agustinb98 <agustinb1423@hotmail.com>
2023-05-12 23:36:30 -0300

    Agregar capa de negocio alta y baja snacks
    Co-Authored-By: htopo <49622428+htopo@users.noreply.github.com>

---
+++ Codigo/ArenaGestor/ArenaGestor.Business/SnacksService.cs
@@ -0,0 +1,39 @@
+﻿using ArenaGestor.BusinessInterface;
+using ArenaGestor.DataAccessInterface;
+using ArenaGestor.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGestor.Business
+{
+    public class SnacksService : ISnacksService
+    {
+        private readonly ISnacksManagement snacksManagement;
+
+        public SnacksService(ISnacksManagement snacksManagement)
+        {
+            this.snacksManagement = snacksManagement;
+        }
+
+        public Snack InsertSnack(Snack snack)
+        {
+            snacksManagement.InsertSnack(snack);
+            snacksManagement.Save();
+            return snack;
+        }
+
+        public void DeleteSnack(string snackName)
+        {
+            snacksManagement.DeleteSnack(snackName);
+            snacksManagement.Save();
+        }
+
+        public List<Snack> GetAllSnacks()
+        {
+            return snacksManagement.GetAllSnacks();
+        }
+    }
+}
