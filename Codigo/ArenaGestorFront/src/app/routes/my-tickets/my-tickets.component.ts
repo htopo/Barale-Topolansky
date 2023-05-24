9a8d216c1f176e73c1a8a0507f2683b8d56ed520
Agustinb98 <agustinb1423@hotmail.com>
2023-05-23 22:10:18 -0300

    front compra snacks
    Co-Authored-By: htopo <49622428+htopo@users.noreply.github.com>

--- Codigo/ArenaGestorFront/src/app/routes/my-tickets/my-tickets.component.ts
+++ Codigo/ArenaGestorFront/src/app/routes/my-tickets/my-tickets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SnackBuyLineDto } from 'src/app/models/Snacks/SnackBuyLineDto';
 import { TicketGetTicketResultDto } from 'src/app/models/Tickets/TicketGetTicketResultDto';
 import { TicketsService } from 'src/app/services/tickets.service';
 
@@ -9,12 +10,14 @@ import { TicketsService } from 'src/app/services/tickets.service';
 export class MyTicketsComponent implements OnInit {
 
   ticketList: Array<TicketGetTicketResultDto> = new Array();
+  snackList: Array<SnackBuyLineDto> = new Array();
 
   constructor(private ticketService: TicketsService) { }
 
   ngOnInit(): void {
     this.ticketService.GetOfLoggedUser().subscribe(res => {
-      this.ticketList = res;
+      this.ticketList = res.ticketDto;
+      this.snackList = res.snacksDto;
     })
   }
 
